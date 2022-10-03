e5face1d82db06b781283371391ee7a46564db8f
sash04ek <a.ovcharov@afterlogic.com>
2022-10-03 16:48:47 +0300

    Remove outdated EAV-related code - #30cgvqz

--- Module.php
+++ Module.php
@@ -73,13 +73,6 @@ class Module extends \Aurora\System\Module\AbstractModule
 		$this->oApiAliasesManager = new Managers\Aliases\Manager($this);
 		$this->oApiMailingListsManager = new Managers\MailingLists\Manager($this);
 		$this->oApiMtaDomainsManager = new Managers\Domains\Manager($this);
-
-		\Aurora\Modules\Core\Classes\User::extend(
-			self::GetName(),
-			[
-				'TotalQuotaBytes' => array('bigint', self::QUOTA_KILO_MULTIPLIER * self::QUOTA_KILO_MULTIPLIER), // bytes
-			]
-		);
 	}
 
 	protected function getDomainsManager()
